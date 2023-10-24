ef5ed26c078d911592b18af38edd7ddb787d02ea
fano <fhrazanakoto@gmail.com>
2023-10-24 22:45:45 +0300

    set new team template

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -9,13 +9,25 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('IS_AUTHENTICATED')]
 #[Route('/teams')]
+/**
+ * Handle things relative to teams.
+ */
 class TeamController extends AbstractController
 {
     #[Route('/', name: 'app_team_index')]
+    /**
+     * Index access teams listing.
+     */
     public function index(): Response
     {
         return $this->render('team/index.html.twig', [
             'controller_name' => 'TeamController',
         ]);
+    }
+
+    #[Route('/new', name: 'app_team_new')]
+    public function newTeam(): Response
+    {
+        return $this->render('team/new.html.twig', []);
     }
 }
