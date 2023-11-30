58e9167f16d0cd767e4ee2bba0ee90d438cbac1d
fano <fhrazanakoto@gmail.com>
2023-11-30 23:31:07 +0300

    remove commented out code in repositories

--- src/Repository/TeamRepository.php
+++ src/Repository/TeamRepository.php
@@ -22,31 +22,6 @@ class TeamRepository extends ServiceEntityRepository
         parent::__construct($registry, Team::class);
     }
 
-    //    /**
-    //     * @return Team[] Returns an array of Team objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('t.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Team
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
-
     public function getPaginatedTeams($page = 1, $postsPerPage = 1)
     {
         $query = $this->createQueryBuilder('tm')
